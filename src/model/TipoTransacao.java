e9afb9285cdc98b6e0df5984e9f1027b4390acef
Rogerir <rogerio.rr2105@gmail.com>
2026-06-05 00:50:35 -0300

    feat: adiciona modelo de Transacao e enum TipoTransacao

---
+++ src/model/TipoTransacao.java
@@ -0,0 +1,6 @@
+package model;
+
+public class TipoTransacao {
+    RECEITA,
+    DESPESA
+}
---
+++ src/model/Transacao.java
@@ -0,0 +1,79 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Transacao {
+    private Integer id;
+    private String descricao;
+    private double valor;
+    private TipoTransacao tipo;
+    private LocalDate data;
+
+    public Transacao(){
+
+    }
+
+    public Transacao(String descricao, double valor, TipoTransacao tipo, LocalDate data) {
+        this.descricao = descricao;
+        this.valor = valor;
+        this.tipo = tipo;
+        this.data = data;
+
+
+    }
+
+    public Transacao(String descricao, Integer id, double valor,TipoTransacao tipo, LocalDate data ){
+        this.id = id;
+        this.descricao = descricao;
+        this.valor = valor;
+        this.tipo = tipo;
+        this.data = data;
+
+    }
+    public Integer getId(){
+        return id;
+    }
+
+    public void serId(Integer id){
+        this.id = id;
+    }
+
+    public String getDescricao(){
+        return descricao;
+    }
+
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
+
+    public double getValor(){
+        return valor;
+    }
+    public TipoTransacao getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoTransacao tipo) {
+        this.tipo = tipo;
+    }
+
+    public java.time.LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Transacao{" +
+                "id=" + id +
+                ", descricao='" + descricao + '\'' +
+                ", valor=" + valor +
+                ", tipo=" + tipo +
+                ", data=" + data +
+                '}';
+    }
+
+}
